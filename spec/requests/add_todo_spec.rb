7be4dd5f5de4100ab66f50871fcbecefea711a93
Peter Sumskas <peter.sumskas@envato.com>
2018-03-27 16:44:09 +1100

    Passed: ./spec/requests/add_todo_spec.rb

--- spec/requests/add_todo_spec.rb
+++ spec/requests/add_todo_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'add todo', type: :request do
 
       expect(last_response.status).to be 201
       expect(last_event(todo_id)).to be_a TodoAdded
-      expect(last_event(todo_id).aggregate_id).to eq todo_id
+      expect(last_event(todo_id).stream_id).to eq todo_id
       expect(last_event(todo_id).body).to eq(
         'title' => '2000 squats',
         'description' => 'Leg day.',
